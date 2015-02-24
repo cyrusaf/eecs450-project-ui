b8d0a1e31c425f3ef013960a8807bff13757437b
Cyrus Forbes <cyrusaf@cyrus-laptop-15.(none)>
2015-02-23 22:35:38 -0600

    finalized

--- js/controllers.js
+++ js/controllers.js
@@ -2,15 +2,25 @@ var controllers = angular.module('controllers', []);
 
 controllers.controller('itemController', ['$scope', '$http', function ($scope, $http) {
 
-	$scope.apps = [
-		{
-			name: 'Mail',
-			id: 'com.apple.mail'
-		},
-		{
-			name: 'Calendar',
-			id: 'com.apple.calendar'
-		}
-	];
+	$scope.apps = [];
+	getApps($scope, $http);
+
+	$scope.addApp = function() {
+		$http.post('http://localhost:3001/items', $scope.create).success(function(data, status, headers, config) {
+			getApps($scope, $http);
+		});
+	}
+
+	$scope.removeApp = function() {
+		$http.post('http://localhost:3001/items/delete', $scope.remove).success(function(data, status, headers, config) {
+			getApps($scope, $http);
+		});
+	}
 
 }]);
+
+function getApps($scope, $http) {
+	$http.get('http://localhost:3001/items').success(function(data, status, headers, config) {
+		$scope.apps = data;
+	});		
+}
