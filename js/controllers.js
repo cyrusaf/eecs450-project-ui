86ff3efce63de339e7a5ad589671d4ecfa057ec4
Cyrus Forbes <cyrusaf@cyrus-laptop-15.(none)>
2015-03-18 13:28:22 -0500

    final commit

--- js/controllers.js
+++ js/controllers.js
@@ -4,16 +4,37 @@ controllers.controller('itemController', ['$scope', '$http', function ($scope, $
 
 	$scope.ip = "http://localhost:3001"
 	$scope.apps = [];
+	$scope.exts = [".pdf", ".txt", ".docx", ".xlsx", ".jpg", ".png"];
 	getApps($scope, $http);
 
-	$scope.addApp = function() {
-		$http.post($scope.ip + '/items', $scope.create).success(function(data, status, headers, config) {
+	$scope.removeApp = function() {
+		$http.post($scope.ip + '/items/delete', $scope.remove).success(function(data, status, headers, config) {
 			getApps($scope, $http);
 		});
 	}
 
-	$scope.removeApp = function() {
-		$http.post($scope.ip + '/items/delete', $scope.remove).success(function(data, status, headers, config) {
+	$scope.toggleExt = function(app, ext) {
+		if (app.exts.indexOf(ext) == -1) {
+			$scope.addExt(app, ext);
+		} else {
+			$scope.removeExt(app, ext);
+		}
+	}
+
+	$scope.addExt = function(app, ext) {
+		var temp = app;
+		console.log(ext);
+		temp.exts.push(ext);
+		$http.put($scope.ip + '/items', temp).success(function(data, status, headers, config) {
+			getApps($scope, $http);
+		});
+	}
+
+	$scope.removeExt = function(app, ext) {
+		var temp = app;
+		console.log(ext);
+		temp.exts.splice(temp.exts.indexOf(ext), 1);
+		$http.put($scope.ip + '/items', temp).success(function(data, status, headers, config) {
 			getApps($scope, $http);
 		});
 	}
@@ -23,5 +44,27 @@ controllers.controller('itemController', ['$scope', '$http', function ($scope, $
 function getApps($scope, $http) {
 	$http.get($scope.ip + '/items').success(function(data, status, headers, config) {
 		$scope.apps = data;
+
+		// Get exts
+		//getExts($scope);
 	});		
+}
+
+function getExts($scope) {
+	$scope.exts = $scope.apps.reduce(function(prev, curr, index) {
+		if (index == 1) {
+			var ext = curr.exts[i];
+			if (prev.exts.indexOf(ext) == -1) {
+				prev.exts.push(ext);
+			}
+			return prev.exts
+		}
+		for (var i = 0; i < curr.exts.length; i++) {
+			var ext = curr.exts[i];
+			if (prev.indexOf(ext) == -1) {
+				prev.push(ext);
+			}
+		}
+		return prev
+	});
 }
